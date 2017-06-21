11b24c2a948b01cc3fa931059d637b98d91fa3a2
RunshaHun <runsh.hu@gmail.com>
2017-06-21 16:56:13 +0100

    write

--- fullexample/example01.go
+++ fullexample/example01.go
@@ -67,8 +67,6 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
         //Handle different functions
         if function == "init" {
                 return t.Init(stub, "init", args)
-        } else if function == "write" {
-                return t.write(stub, args)
         } else if function == "queryMatchTest" {
                 logger.Info("--->queryMatchTest invoked")
                 return t.queryMatchTest(stub, args)
